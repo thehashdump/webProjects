e2d505bcb5bf1f55b16734f244b142f383c62e83
Uzair Khalid <thehashdump@gmail.com>
2022-06-20 16:51:52 +0500

    change in package.json file

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const exp = require("constants")
 const express = require("express")
 const app = express()
-const port = process.env.PORT ||8000
+const port = process.env.PORT || 8000
 const path = require('path')
 const hbs = require("hbs")
 
